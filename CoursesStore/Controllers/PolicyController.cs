09726427873769b7e29cd4dda96b9ce03938cddc
Dr.Powar <vini.wi555@gmail.com>
2023-08-23 12:49:56 +0300

    Оновлення файлу _Layout (дві кнопки в хедері, і функціонал для кнопок у футері).

---
+++ CoursesStore/Controllers/PolicyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursesStore.Controllers
+{
+    public class PolicyController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View("Privacy policy");
+        }
+        public IActionResult Refund()
+
+        {
+            return View("Refund policy");
+        }
+        public IActionResult TermsOfService()
+        {
+            return View("TermsOfService");
+        }
+    }
+}
